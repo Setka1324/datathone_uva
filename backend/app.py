706d47ab21ee8b4ee6d14f398e3e49045bae33c3
sKy9-99-9 <76662409+sKy9-99-9@users.noreply.github.com>
2025-05-13 17:25:31 +0300

    .

--- backend/app.py
+++ backend/app.py
@@ -14,7 +14,7 @@ def create_app():
     app = Flask(__name__) # Create the app instance inside the factory
     CORS(
         app,
-        resources={r"/api/*": {"origins": "http://localhost:3000"}},
+        resources={r"/api/*": {"origins": "http://localhost:3001"}},
         methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
         allow_headers=["Content-Type", "Authorization"],
         supports_credentials=True
